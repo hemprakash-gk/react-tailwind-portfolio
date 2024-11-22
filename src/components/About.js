46248ad813167d740837b8424dbe2f23b118848a
Hemprakash GK <75492419+hemprakash-gk@users.noreply.github.com>
2024-11-22 20:03:30 +0530

    Update About.js

--- src/components/About.js
+++ src/components/About.js
@@ -2,9 +2,8 @@ import AboutImg from '../assets/about.png';
 
 export default function About () {
     const config  = {
-        line1: 'Hi, My name is JV Logesh. I am a Full stack web developer. I built beautiful Websites with React.js , Tailwind CSS + Bootstrap',
-        line2: 'I am proficient in Frontend skills like React.js, Redux, Redux Tool Kit, Axios, Tailwind CSS, SaSS, Css3 and many more.',
-        line3: 'In backend I know Node.js, Express.js, MongoDB, and Mongoose'
+        line1: 'Hi, My name is Hemprakash. I am a Full stack web developer. I built beautiful Websites with React.js , Tailwind CSS + Bootstrap',
+        line2: 'I am proficient in Java , SpringBoot, MySql',
     }
 
     return <section className='flex flex-col md:flex-row bg-secondary px-5' id='about'>
@@ -16,7 +15,6 @@ export default function About () {
                 <h1 className='text-4xl border-b-4 border-primary mb-5 w-[170px] font-bold'>About Me</h1>
                 <p className='pb-5'>{config.line1}</p>
                 <p className='pb-5'>{config.line2}</p>
-                <p className='pb-5'>{config.line3}</p>
             </div>
         </div>
     </section>
